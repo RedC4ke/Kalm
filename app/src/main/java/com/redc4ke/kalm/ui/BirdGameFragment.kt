3e596143b5dc42f34358ffe797bc8471dea2f17b
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-05-28 23:31:46 +0200

    v0.0.6 - some changes for showcase

--- app/src/main/java/com/redc4ke/kalm/ui/BirdGameFragment.kt
+++ app/src/main/java/com/redc4ke/kalm/ui/BirdGameFragment.kt
@@ -13,17 +13,20 @@ import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
 import com.redc4ke.kalm.R
 import com.redc4ke.kalm.databinding.FragmentBirdGameBinding
-import com.redc4ke.kalm.ui.base.BaseFragment
+import com.redc4ke.kalm.ui.base.GameFragment
 import nl.dionsegijn.konfetti.models.Shape
 import nl.dionsegijn.konfetti.models.Size
 
-class BirdGameFragment : BaseFragment<FragmentBirdGameBinding>() {
+class BirdGameFragment : GameFragment<FragmentBirdGameBinding>() {
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentBirdGameBinding
         get() = FragmentBirdGameBinding::inflate
     private var draggedView: View? = null
     private var draggedColor: String? = null
     private var score = 0
+    override val directions = arrayOf(
+        R.id.action_BirdGame_DrawGame
+    )
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -164,7 +167,7 @@ class BirdGameFragment : BaseFragment<FragmentBirdGameBinding>() {
             )
             .streamFor(300, 5000L)
 
-        CompletedFragment(getString(R.string.birdgame_completed))
+        CompletedFragment(getString(R.string.birdgame_completed), this)
             .show(parentFragmentManager, "complete")
     }
 
--- app/src/main/java/com/redc4ke/kalm/ui/base/BaseFragment.kt
+++ app/src/main/java/com/redc4ke/kalm/ui/base/BaseFragment.kt
@@ -29,3 +29,9 @@ abstract class BaseFragment<VB: ViewBinding>: Fragment() {
         _binding = null
     }
 }
+
+abstract class GameFragment<VB: ViewBinding> : BaseFragment<VB>() {
+
+    abstract val directions: Array<Int>
+
+}
